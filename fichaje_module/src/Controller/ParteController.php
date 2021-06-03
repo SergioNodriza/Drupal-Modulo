f724405c141c964beb09b10aa3cd812de86e8279
SergioNodriza <sergio.gomez@nodrizatech.com>
2021-06-03 12:08:23 +0200

    Week Filter Separated

--- fichaje_module/src/Controller/ParteController.php
+++ fichaje_module/src/Controller/ParteController.php
@@ -27,62 +27,28 @@ class ParteController extends ControllerBase {
   public function parte($empresaName)
   {
     Drupal::service("router.builder")->rebuild();
-    $user = Drupal::currentUser();
-    $connection = Database::getConnection();
-
     $this->redirectByFilters($_POST['link'], $_POST['date_filter'], $_POST['week_filter']);
 
-    if (!$empresaName) {$empresaName = '%';}
-
+    $user = Drupal::currentUser();
+    $connection = Database::getConnection();
     $date_filter = Drupal::request()->get('date_filter');
     $week_filter = Drupal::request()->get('week_filter');
 
     $fichajes = $this->getFichajesByFilters($connection, $user, $empresaName, $date_filter, $week_filter);
-
-    $arrayWeeks = array();
-    foreach($fichajes as $key => $fichaje) {
-
-      $date = $fichaje['date'];
-      $week = date('W', strtotime($date));
-      $fichajes[$key]['date'] = $this->timeService->formatDate($date);
-
-      if(!isset($arrayWeeks[$week]) ) {
-        $arrayWeeks[$week]['fichajes'] = array();
-      }
-      $arrayWeeks[$week]['fichajes'][] = $fichajes[$key];
-    }
-
-
-    foreach ($arrayWeeks as $key => $arrayWeek) {
-
-      $totalSeconds = 0;
-      foreach ($arrayWeek['fichajes'] as $key2 => $fichaje) {
-
-        if ($fichaje['type'] === 'Salida') {
-          $totalSeconds += $this->timeService->timeToSeconds($fichaje['time']);
-        }
-      }
-
-      $arrayWeeks[$key]['total'] = $this->timeService->formatSeconds($totalSeconds);
-    }
-
-    if ($empresaName === '%') {
-      $title = 'Parte de Horas General';
-    } else {
-      $title = 'Parte de Horas de ' . $empresaName;
-    }
+    $arrayWeeks = $this->groupFichajesByWeekAndFormat($fichajes);
+    $arrayCompleted = $this->getTotalsByWeek($arrayWeeks);
 
     return array(
-      '#title' => $title,
+      '#title' => $this->formatTitle($empresaName, $date_filter, $week_filter),
       '#theme' => 'usuario_fichajes',
-      '#results' => $arrayWeeks,
+      '#results' => $arrayCompleted,
       '#buttons' => $this->buttons($connection),
       '#route' => Drupal::routeMatch()->getParameter('empresaName') ?? 'General'
     );
   }
 
 
-  public function redirectByFilters($linkFilter, $dateFilter, $week = false)
+  public function redirectByFilters($linkFilter, $dateFilter, $weekFilter = false)
   {
     $url = null;
 
@@ -97,30 +63,17 @@ class ParteController extends ControllerBase {
     if ($dateFilter) {
       $url .= '?date_filter=' . $dateFilter;
 
-      if ($week) {
+      if ($weekFilter) {
         $url .= '&week_filter=' . date('W', strtotime($dateFilter));
       }
-    } elseif ($week) {
-      $url .= '?week_filter=' . date('W', strtotime($dateFilter));
+    } elseif ($weekFilter) {
+      $url .= '?week_filter=' . date('W', strtotime(date('Y-m-d')));
     }
 
     if ($url) {
       $response = new RedirectResponse($url);
       $response->send();
     }
-  }
-
-
-  public function buttons($connection)
-  {
-
-    $empresasIds = $this->queryService->queryEmpresasIds($connection);
-
-    if (Drupal::request()->getRequestUri() !== '/parte') {
-      return $this->buttonMakerService->makeButtons($empresasIds, true);
-    }
-
-    return $this->buttonMakerService->makeButtons($empresasIds);
   }
 
   public function getFichajesByFilters($connection, $user, $empresaName, $date_filter, $week_filter)
@@ -141,18 +94,93 @@ class ParteController extends ControllerBase {
         $date = date('Y-m-d');
       }
 
-      $week = date('W', strtotime($date));
+
       $year = date('Y', strtotime($date));
 
       $daysWeek = [];
-      for($day=1; $day<8; $day++)
-      {
-        $daysWeek[] = date('Y-m-d', strtotime($year."W".$week.$day));
+      for($day=1; $day<8; $day++) {
+        $daysWeek[] = date('Y-m-d', strtotime($year."W".$week_filter.$day));
       }
 
       $params['week_filter'] = $daysWeek;
     }
 
     return $this->queryService->queryFichajesUsuario($connection, $params);
+  }
+
+  public function groupFichajesByWeekAndFormat($fichajes) {
+
+    $arrayWeeks = array();
+    foreach($fichajes as $key => $fichaje) {
+
+      $date = $fichaje['date'];
+      $week = date('W', strtotime($date));
+      $fichajes[$key]['date'] = $this->timeService->formatDate($date);
+
+      if(!isset($arrayWeeks[$week]) ) {
+        $arrayWeeks[$week]['fichajes'] = array();
+      }
+      $arrayWeeks[$week]['fichajes'][] = $fichajes[$key];
+    }
+
+    return $arrayWeeks;
+  }
+
+  public function getTotalsByWeek($arrayWeeks) {
+
+    foreach ($arrayWeeks as $key => $arrayWeek) {
+
+      $totalSeconds = 0;
+      foreach ($arrayWeek['fichajes'] as $key2 => $fichaje) {
+
+        if ($fichaje['type'] === 'Salida') {
+          $totalSeconds += $this->timeService->timeToSeconds($fichaje['time']);
+        }
+      }
+
+      $arrayWeeks[$key]['total'] = $this->timeService->formatSeconds($totalSeconds);
+    }
+    return $arrayWeeks;
+  }
+
+  public function formatTitle($empresaName, $date_filter, $week_filter) {
+
+    $title = 'Parte de Horas ';
+
+    if (!$empresaName) {
+      $title .= 'General';
+    } else {
+      $title .= 'de ' . $empresaName;
+    }
+
+    if ($date_filter && !$week_filter) {
+      $title .= ' | Filtro día ' . date('d/m/Y', strtotime($date_filter));
+    }
+
+    if ($week_filter) {
+
+      if ($date_filter) {
+        $date = $date_filter;
+      }
+      else {
+        $date = date('Y-m-d');
+      }
+
+      $title .= ' | Filtro semana del ' . date('d/m/Y', strtotime($date));
+    }
+
+    return $title;
+  }
+
+  public function buttons($connection)
+  {
+
+    $empresasIds = $this->queryService->queryEmpresasIds($connection);
+
+    if (Drupal::request()->getRequestUri() !== '/parte') {
+      return $this->buttonMakerService->makeButtons($empresasIds, true);
+    }
+
+    return $this->buttonMakerService->makeButtons($empresasIds);
   }
 }
